0dfcc9511931badf624299e5209701c21a03954d
13416877866 <2638706597@qq.com>
2021-11-13 12:33:24 +0800

    30

--- src/object/clone.js
+++ src/object/clone.js
@@ -43,3 +43,26 @@ function deepClone1(target){
     let data=JSON.parse(str);
     return data;
 }
+
+
+
+
+function deepClone2(target){
+    // 检测一下数据类型
+    if(typeof target==='object'&&target!==null){
+    //    创造一个容器
+    const result=Array.isArray(target)?[]:{};
+    // 遍历数组
+    for(let key in target){
+        // 检测该属性是否为对象本身的属性（不能拷贝原型对象的属性)
+        if(target.hasOwnProperty(key)){
+            //拷贝
+            result[key]=deepClone2(target[key]);
+
+        }
+    }
+    return result;
+    }else{
+        return target;
+    }
+}
