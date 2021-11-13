3d819196137f1f54e24438a6ca83064f7bfb484e
13416877866 <2638706597@qq.com>
2021-11-13 14:19:48 +0800

    31

--- src/object/clone.js
+++ src/object/clone.js
@@ -66,3 +66,31 @@ function deepClone2(target){
         return target;
     }
 }
+
+
+function deepClone3(target,map=new Map()){
+    // 检测数据的类型
+    if(typeof target ==='object' && target !==null){
+        // 克隆数据之前，通过判断，数据之前是否有克隆过
+        let cache=map.get(target);
+        if(cache){
+            return cache;
+        }
+        // 创建一个容器
+        const result =Array.isArray(target)?[]:{};
+        // 将新的结果存入到数据库中
+        map.set(target,result);
+        // 遍历对象
+        for(let key in target){
+            // 检测该属性是否为对象本身的属性（不能拷贝原型对象的属性）
+            if(target.hasOwnProperty(key)){
+                // 拷贝
+                result[key]=deepClone3(target[key],map);
+            }
+        }
+        return result;
+    }else{
+        return target;
+    }
+    
+}
