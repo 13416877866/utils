6aabc124ef03081ce033bcbe5c6b02b84263ad96
13416877866 <2638706597@qq.com>
2021-11-14 11:37:18 +0800

    35

--- src/string/index.js
+++ src/string/index.js
@@ -18,3 +18,29 @@ function palindrome(str){
 function truncate(str,size){
     return str.slice(0,size)+'...';
 }
+
+
+
+function addEventListener(el,type,fn,selector){
+    // 判断el的类型
+    if(typeof el==='string'){
+        el=document.querySelector(el);
+    }
+//    事件绑定
+// 若没有传递子元素的选择器，则给el元素绑定事件
+if(!selector){
+    el.addEventListener(type,fn);
+}else{
+    el.addEventListener(type,function(e){
+        //获取点击的目标事件源
+        const target=e.target;
+        // 判断选择器与目标元素是否相符合
+        if(target.matches(selector)){
+            // 若符合则调用回调
+            fn.call(target,e);
+        }
+    });
+}
+}
+
+
