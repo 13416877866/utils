009740925ed345aca47c466e292c5adfc1b4bce5
13416877866 <2638706597@qq.com>
2021-11-14 14:35:50 +0800

    38

--- src/pub-sub/index.js
+++ src/pub-sub/index.js
@@ -21,6 +21,7 @@ PubSub.subscribe=function(channel,callback)
             [token]:callback
         }
     }
+    return token;
 }
 
 /* 发布消息 */
@@ -30,5 +31,25 @@ PubSub.publish=function(channel,data){
         Object.values(this.callbacks[channel]).forEach(callback=>{
             callback(data);
         })
+    }
+}
+
+/* 取消订阅 */
+PubSub.unsubscribe=function(flag){
+    // 如果flag为undefined则清空所有订阅
+    if(flag===undefined){
+        this.callbacks={};
+    }else if(typeof flag==='string'){
+        // 判断是否为token_开头
+        if(flag.indexOf('token_')===0){
+            // 如果是表明是一个订阅id
+           let callbackObj= Object.values(this.callbacks).find(obj=>obj.hasOwnProperty(flag));
+           if(callbackObj){
+               delete callbackObj[flag];
+           }
+        }else{
+            // 表明是一个频道的名称
+            delete this.callbacks[flag];
+        }
     }
 }
